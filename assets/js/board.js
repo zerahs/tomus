587c4f75943522ae269b2f6e1f9c51ddcc8f3a17
zerahs <sebastien.zerah@gmail.com>
2019-02-20 22:44:24 +0100

    Disabled and focus next row

--- assets/js/board.js
+++ assets/js/board.js
@@ -10,10 +10,15 @@ function isLetter(c) {
  	// return c.length === 1 && c.match(/[a-z]/i);
 }
 
+
+// TODO
+// Set all squares disabled except the one with focus ?
+// Or set all squares disabled except current row
 function Square(props) {
 
 	return (
 		<input 
+			disabled={props.disabled}
 			type="text"
 			className="square"
 			onInput={props.onInput}
@@ -26,9 +31,71 @@ class Row extends React.Component {
 
 	renderSquare(i) {
 		return <Square
+			key={i}
+			onKeyDown={this.props.onKeyDown}
+			onInput={this.props.onInput}
+			disabled={this.props.disabled}
+		/>;
+	}
+
+	render() {
+		let squares = [];
+		for(let i=0;i<this.props.nbCols; i++){
+			squares.push(this.renderSquare(i));
+		}
+		return (
+			<div className="board-row">
+				{squares}
+			</div>
+		);
+	}
+}
+
+class Board extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			nbCols: 9,
+			nbRows: 6,
+			currentRow: 0,
+		}
+
+
+		// This binding is necessary to make 'this' work in the callback
+		this.onKeyDown = this.onKeyDown.bind(this);
+	}
+
+	focusFirstAvailableInput() {
+		let input = $(".board-row").eq(this.state.currentRow).find('input:enabled:first');
+		input.focus();
+	}
+
+	componentDidUpdate(prevProps) {
+		this.focusFirstAvailableInput();
+	}
+	componentDidMount(prevProps) {
+		this.focusFirstAvailableInput();
+	}
+
+	renderRow(i) {
+		return <Row
+			nbCols={this.state.nbCols}
+			key={i}
 			onKeyDown={this.onKeyDown}
 			onInput={this.onInput}
+			disabled={i!=this.state.currentRow}
 		/>;
+	}
+
+	onInput(e) {
+		let target = $(e.target);
+		let data = e.nativeEvent.data;
+		if(isLetter(data)){
+			target.val(data.toUpperCase());
+			target.next().focus();
+		}
 	}
 
 	onKeyDown(e) {
@@ -42,68 +109,43 @@ class Row extends React.Component {
 		if(isLetter(character))
 			return true;
 		
-		console.log(code);
 		// Backspace or left arrow
 		if(code==8 || code==37){
 			let target = $(e.target);
 			target.prev().focus();
 		}
 		// Right arrow
-		if(code==39){
+		else if(code==39){
 			let target = $(e.target);
 			target.next().focus();
 		}
 		// Enter
-		if(code==13){
-			// TODO submit word
-			// This will have to be done in another class (board or game ?)
+		else if(code==13){
+			this.validateAndSubmit();
 		}
 		e.preventDefault();
 		e.stopPropagation();
 		return false;
 	}
 
-	/*
-	* Replace text by input and focus next square
-	*/
-	onInput(e) {
-		console.log('input');
-		let target = $(e.target);
-		let data = e.nativeEvent.data;
-		if(isLetter(data)){
-			target.val(data.toUpperCase());
-			target.next().focus();
+	validateAndSubmit() {
+		const newRow = this.state.currentRow + 1;
+		if(newRow >= this.state.nbRows){
+			//END GAME
 		}
+		this.setState({currentRow:newRow});
+		let temp = $('input:enabled:first');
+		temp.focus();
 	}
 
 	render() {
-		return (
-			<div className="board-row">
-				{this.renderSquare()}
-				{this.renderSquare()}
-				{this.renderSquare()}
-				{this.renderSquare()}
-				{this.renderSquare()}
-				{this.renderSquare()}
-			</div>
-		);
-	}
-}
-
-class Board extends React.Component {
-
-	renderRow(i) {
-		return <Row />;
-	}
-
-	render() {
-
+		let rows = [];
+		for(let i=0; i<this.state.nbRows;i++){
+			rows.push(this.renderRow(i));
+		}
 		return (
 			<div className="board">
-				{this.renderRow()}
-				{this.renderRow()}
-				{this.renderRow()}
-				{this.renderRow()}
+				{rows}
 			</div>
 		);
 	}
